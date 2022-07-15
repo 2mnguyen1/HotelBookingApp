39b2ffa249877da8eb16d6c04c38ebb48c7dc4b6
2mnguyen1 <2mnguyen1@gmail.com>
2022-07-15 16:32:26 -0700

    git ignore

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
 app.listen(8800, () => {
-    console.log("Connected to 8800 port");
-})
+  console.log("Connected to 8800 port");
+});
+
