720b4eebfe3261a0f5463a7b49006016a93f8e87
2mnguyen1 <2mnguyen1@gmail.com>
2022-07-15 17:26:12 -0700

    setting up router

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./router/auth.js";
+import hotelsRoute from "./router/hotels.js";
+import roomsRoute from "./router/rooms.js";
+import usersRoute from "./router/users.js";
 
 const app = express();
 dotenv.config();
@@ -21,7 +25,12 @@ mongoose.connection.on("connected", () => {
   console.log("mongoDB connected!");
 });
 
+// middlewares
 
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
 
 app.listen(8800, () => {
   connect();
