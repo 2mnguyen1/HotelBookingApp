8763b1cb0159d9ef472ebc80d4aa6c4e4986cf5a
2mnguyen1 <2mnguyen1@gmail.com>
2022-07-16 20:26:20 -0700

    adding login api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,14 +1,37 @@
 import User from "../models/Users.js";
+import bcrypt from "bcrypt";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     });
     await newUser.save();
-    res.status(200).send("User has been registered");
+    res.status(200).send("User has been registered.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) return next(createError(404, "User not found!"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or username!"));
+
+    res.status(200).send(user);
   } catch (err) {
     next(err);
   }
