3c414a2ce21ee55bb9fb1fd4bc351139fbde65ba
2mnguyen1 <2mnguyen1@gmail.com>
2022-07-22 15:30:51 -0700

    adding createRoom api

---
+++ api/controllers/room.js
@@ -0,0 +1,15 @@
+import Room from "../models/Rooms.js";
+import Hotel from "../models/Hotels.js";
+import { createError } from "../utils/error.js";
+
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelId;
+  const newRoom = new Room(req.body);
+  try {
+    const savedRoom = await newRoom.save();
+    await Hotel.findByIdAndUpdate(hotelId, { $push: { rooms: savedRoom._id } });
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
