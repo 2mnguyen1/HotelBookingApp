70e5215057f1a47442a64384f11ea51852b42332
2mnguyen1 <2mnguyen1@gmail.com>
2022-07-21 17:25:19 -0700

    adding verifing user

--- api/router/users.js
+++ api/router/users.js
@@ -5,10 +5,17 @@ import {
   getUser,
   getAllUsers,
 } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req, res) => {
+  res.send("You are authenicated");
+});
+
+router.get("/checkuser/:id", verifyUser , (req, res) => {
+  res.send("You can delete this account!!");
+});
 
 // UPDATE
 router.put("/:id", updateUser);
