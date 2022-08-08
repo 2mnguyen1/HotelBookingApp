72ea3ae1f4330fbe6c39ff17a380da653ab459b7
2mnguyen1 <2mnguyen1@gmail.com>
2022-08-08 14:58:37 -0700

    adding feature components

---
+++ client/src/components/feature/Feature.jsx
@@ -0,0 +1,38 @@
+import "./feature.css";
+
+export default function Feature() {
+    return (
+        <div className="feature-container">
+            <div className="feature-item">
+                <img
+                    src="https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    alt=""
+                />
+                <div className="feature-title">
+                    <h1>Dublin</h1>
+                    <h2>123 properties</h2>
+                </div>
+            </div>
+            <div className="feature-item">
+                <img
+                    src="https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    alt=""
+                />
+                <div className="feature-title">
+                    <h1>Dublin</h1>
+                    <h2>123 properties</h2>
+                </div>
+            </div>
+            <div className="feature-item">
+                <img
+                    src="https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    alt=""
+                />
+                <div className="feature-title">
+                    <h1>Dublin</h1>
+                    <h2>123 properties</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
-import "./home.css"
-import Navbar from "../../components/navbar/Navbar"
-import Header from "../../components/header/Header"
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import Feature from "../../components/feature/Feature";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <div className="home-container">
+                <Feature />
+            </div>
+        </div>
+    );
 }
