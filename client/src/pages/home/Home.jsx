82285abeb206264b9199b45441656c35de9a7180
2mnguyen1 <2mnguyen1@gmail.com>
2022-08-09 14:03:04 -0700

    finished homepage list

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Feature from "../../components/feature/Feature";
 import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperty from "../../components/featuredProperty/FeaturedProperty";
 
 export default function Home() {
     return (
@@ -14,7 +15,7 @@ export default function Home() {
                 <h1 className="home-title">Browse by property type</h1>
                 <PropertyList />
                 <h1 className="home-title">Homes guests love</h1>
-                
+                <FeaturedProperty />
             </div>
         </div>
     );
