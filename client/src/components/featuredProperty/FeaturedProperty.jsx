e434150fc7f89397cc6c1d2f742b50ef0fdb79dc
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-08-25 15:52:28 -0700

    chaning getAllHotels api by adding query and limit to

--- client/src/components/featuredProperty/FeaturedProperty.jsx
+++ client/src/components/featuredProperty/FeaturedProperty.jsx
@@ -1,36 +1,10 @@
 import "./featuredProperty.css";
+import useFetch from "../../hooks/useFetch";
 
 export default function FeaturedProperty() {
+    const { data, loading, error } = useFetch("/hotels?featured=true");
     return (
         <div className="featured-property">
-            <div className="featured-property-item">
-                <img
-                    src="https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-                    alt=""
-                    className="fp-item-image"
-                />
-                <span className="fp-name">Aparthotel Stare Miasto</span>
-                <span className="fp-city">Madrid</span>
-                <span className="fp-cost">Starting from $120</span>
-                <div className="fp-rating">
-                    <button>8.9</button>
-                    <span>Exellent</span>
-                </div>
-            </div>
-            <div className="featured-property-item">
-                <img
-                    src="https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-                    alt=""
-                    className="fp-item-image"
-                />
-                <span className="fp-name">Aparthotel Stare Miasto</span>
-                <span className="fp-city">Madrid</span>
-                <span className="fp-cost">Starting from $120</span>
-                <div className="fp-rating">
-                    <button>8.9</button>
-                    <span>Exellent</span>
-                </div>
-            </div>
             <div className="featured-property-item">
                 <img
                     src="https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
