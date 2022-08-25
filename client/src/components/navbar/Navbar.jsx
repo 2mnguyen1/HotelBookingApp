093356c9f2b6107a344cbd6c785852775af9e2af
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-08-25 15:59:52 -0700

    adding link to navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import "./navbar.css"
-
+import "./navbar.css";
+import { Link} from "react-router-dom"
 export default function Navbar() {
-  return (
-      <div className="navbar">
-          <div className="navbar-container">
-              <span className="logo">minhbooking</span>
-              <div className="nav-items">
-                  <button className="nav-button">Register</button>
-                  <button className="nav-button">Login</button>
-              </div>
-          </div>
-      </div>
-  );
+    return (
+        <div className="navbar">
+            <div className="navbar-container">
+                <Link to="/" style={{color: "inherit", textDecoration: "none"}}>
+                    <span className="logo">minhbooking</span>
+                </Link>
+                <div className="nav-items">
+                    <button className="nav-button">Register</button>
+                    <button className="nav-button">Login</button>
+                </div>
+            </div>
+        </div>
+    );
 }
