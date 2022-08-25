ecceb95d2a16bc43bdac07e0d3d35ce3d5006102
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-08-24 21:12:36 -0700

    finished countByCity api request

--- api/index.js
+++ api/index.js
@@ -6,27 +6,29 @@ import hotelsRoute from "./router/hotels.js";
 import roomsRoute from "./router/rooms.js";
 import usersRoute from "./router/users.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
 const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("connected to mongoDB");
-  } catch (error) {
-    throw error;
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("connected to mongoDB");
+    } catch (error) {
+        throw error;
+    }
 };
 
 mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected!");
+    console.log("mongoDB disconnected!");
 });
 mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected!");
+    console.log("mongoDB connected!");
 });
 
 // middlewares
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json()); // to send json request
 
@@ -37,14 +39,14 @@ app.use("/api/users", usersRoute);
 
 // error handling middlewares
 app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong!!";
-  return res.status(errorStatus).json({
-    sucess: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!!";
+    return res.status(errorStatus).json({
+        sucess: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
 });
 
 // NOTE
@@ -52,6 +54,6 @@ app.use((err, req, res, next) => {
 
 // -----------------------------
 app.listen(8800, () => {
-  connect();
-  console.log("connected to 8800 port");
+    connect();
+    console.log("connected to 8800 port");
 });
