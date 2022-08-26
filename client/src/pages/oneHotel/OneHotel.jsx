b360608bcec9efd34597ca4ac9a47c1904967bc3
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-08-25 18:53:10 -0700

    finished fetching OneHotel page

--- client/src/pages/oneHotel/OneHotel.jsx
+++ client/src/pages/oneHotel/OneHotel.jsx
@@ -1,5 +1,6 @@
 import "./oneHotel.css";
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,9 +13,15 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Footer from "../../components/footer/Footer";
 import MailList from "../../components/mailList/MailList";
+import useFetch from "../../hooks/useFetch";
 export default function OneHotel() {
     const [slideNumber, setSlideNumber] = useState(0);
     const [open, setOpen] = useState(false);
+    const location = useLocation();
+    const id = location.pathname.split("/")[2];
+
+    const { data, loading, error } = useFetch(`/hotels/find/${id}`);
+
     const photos = [
         {
             src: "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
@@ -53,100 +60,99 @@ export default function OneHotel() {
         <div>
             <Navbar />
             <Header type="list" />
-            <div className="one-hotel-container">
-                {open && (
-                    <div className="one-hotel-slider">
-                        <div className="slider-wrapper">
-                            <FontAwesomeIcon
-                                icon={faCircleArrowLeft}
-                                className="arrow"
-                                onClick={() => handleMove("left")}
-                            />
-                            <FontAwesomeIcon
-                                icon={faCircleXmark}
-                                className="close"
-                                onClick={() => setOpen(false)}
-                            />
-                            <img
-                                src={photos[slideNumber]?.src}
-                                className="slider-images"
-                            />
-                            <FontAwesomeIcon
-                                icon={faCircleArrowRight}
-                                className="arrow"
-                                onClick={() => handleMove("right")}
-                            />
+            {loading ? (
+                "loading"
+            ) : (
+                <div className="one-hotel-container">
+                    {open && (
+                        <div className="one-hotel-slider">
+                            <div className="slider-wrapper">
+                                <FontAwesomeIcon
+                                    icon={faCircleArrowLeft}
+                                    className="arrow"
+                                    onClick={() => handleMove("left")}
+                                />
+                                <FontAwesomeIcon
+                                    icon={faCircleXmark}
+                                    className="close"
+                                    onClick={() => setOpen(false)}
+                                />
+                                <img
+                                    src={photos[slideNumber]?.src}
+                                    className="slider-images"
+                                />
+                                <FontAwesomeIcon
+                                    icon={faCircleArrowRight}
+                                    className="arrow"
+                                    onClick={() => handleMove("right")}
+                                />
+                            </div>
                         </div>
-                    </div>
-                )}
-                <div className="one-hotel-wrapper">
-                    <button className="btn-bookNow">
-                        Reserve or Book Now!
-                    </button>
-                    <h1 className="one-hotel-title">Grand Hotel</h1>
-                    <div className="one-hotel-address">
-                        <FontAwesomeIcon icon={faLocationDot} />
-                        <span>Elton St 125 New York</span>
-                    </div>
-                    <span className="one-hotel-distance">
-                        Excellent location - 500m from Center
-                    </span>
-                    <span className="one-hotel-price-highlight">
-                        Book a stay over $114 at this property and get free
-                        airport taxi
-                    </span>
-                    <div className="one-hotel-images">
-                        {photos.map((photo, index) => (
-                            <>
-                                <div className="one-hotel-photo-wrapper">
+                    )}
+                    <div className="one-hotel-wrapper">
+                        <button className="btn-bookNow">
+                            Reserve or Book Now!
+                        </button>
+                        <h1 className="one-hotel-title">{data.name}</h1>
+                        <div className="one-hotel-address">
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            <span>{data.address}</span>
+                        </div>
+                        <span className="one-hotel-distance">
+                            Excellent location - {data.distance}m from Center
+                        </span>
+                        <span className="one-hotel-price-highlight">
+                            Book a stay over ${data.cheapestPrice} at this
+                            property and get free airport taxi
+                        </span>
+                        <div className="one-hotel-images">
+                            {data.photos?.map((photo, index) => (
+                                <div
+                                    className="one-hotel-photo-wrapper"
+                                    key={index}
+                                >
                                     <img
                                         onClick={() => handleOpen(index)}
-                                        src={photo.src}
+                                        src={photo}
                                         atl=""
                                     />
                                 </div>
-                            </>
-                        ))}
-                    </div>
-                    <div className="one-hotel-details">
-                        <div className="one-hotel-details-text">
-                            <h1 className="one-hotel-title">
-                                Stay in the heart of Krakow
-                            </h1>
-                            <p className="one-hotel-desc">
-                                Dear lama, your project ideas, teaching, and
-                                front end skills are incredible. You seem like a
-                                front end dev at heart. I think you really shine
-                                when you do React and Firebase/Firestore
-                                projects. I know people keep asking you for
-                                MERN, but from an engineering perspective it’s
-                                not your strength and it’s just too much time to
-                                build with SOLID principles and no spaghetti
-                                code, so the controller ends up basically doing
-                                everything. So I think React/Firebase would be a
-                                good area to specialize.
-                            </p>
+                            ))}
                         </div>
-                        <div className="one-hotel-details-price">
-                            <h1>Perfect for a 9-night stay</h1>
-                            <span>
-                                Location in the real heart of Krakow, this
-                                property has an excellent location score of 9.8!
-                            </span>
-                            <h2>
-                                <b>$945</b> (9 nights)
-                            </h2>
-                            <button>Reverse or Book Now!</button>
+                        <div className="one-hotel-details">
+                            <div className="one-hotel-details-text">
+                                <h1 className="one-hotel-title">
+                                    {data.title}
+                                </h1>
+                                <p className="one-hotel-desc">
+                                    {data.description}
+                                </p>
+                            </div>
+                            <div className="one-hotel-details-price">
+                                <h1>Perfect for a 9-night stay</h1>
+                                {data.rating && (
+                                    <span>
+                                        Location in the real heart of Krakow,
+                                        this property has an excellent location
+                                        score of
+                                        {data.rating}!
+                                    </span>
+                                )}
+                                <h2>
+                                    <b>${data.cheapestPrice * 9}</b> (9 nights)
+                                </h2>
+                                <button>Reverse or Book Now!</button>
+                            </div>
                         </div>
                     </div>
+                    {!open && (
+                        <>
+                            <MailList />
+                            <Footer />
+                        </>
+                    )}
                 </div>
-                {!open && (
-                    <>
-                        <MailList />
-                        <Footer />
-                    </>
-                )}
-            </div>
+            )}
         </div>
     );
 }
