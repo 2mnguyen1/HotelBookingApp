f564712c27336af34afea5ffa809f3568afee5aa
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-08-27 22:55:07 -0700

    fixed AuthContext

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,17 +1,33 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 const INITIAL_STATE = {
-    _id: undefined,
-    username: undefined,
-    isAdmin: false,
+    user: JSON.parse(localStorage.getItem("user")) || nullg,
+    loading: false,
+    error: null,
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 const AuthReducer = (state, action) => {
     switch (action.type) {
-        case "LOGIN":
-            return action.payload;
+        case "LOGIN_START":
+            return {
+                user: null,
+                loading: true,
+                error: null,
+            };
+        case "LOGIN_SUCCESS":
+            return {
+                user: action.payload,
+                loading: false,
+                error: null,
+            };
+        case "LOGIN_FAILURE":
+            return {
+                user: null,
+                loading: false,
+                error: state.payload,
+            };
         case "LOGOUT":
             return INITIAL_STATE;
         default:
@@ -21,12 +37,17 @@ const AuthReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(state.user));
+    }, [state.user]);
+
     return (
         <AuthContext.Provider
             value={{
-                _id: state._id,
-                username: state.username,
-                isAdmin: state.isAdmin,
+                user: state.user,
+                loading: state.loading,
+                error: state.error,
                 dispatch,
             }}
         >
