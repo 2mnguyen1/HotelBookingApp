bc91eefb8a643010d3d068d355ed3d188b466f6a
Minh Nguyen <94807387+2mnguyen1@users.noreply.github.com>
2022-09-03 17:28:40 -0700

    adding get all days in range

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,12 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import useFetch from "../../hooks/useFetch";
+import { SearchContext } from "../../context/SearchContext";
 
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 export default function Reserve({ setOpenReserve, hotelId }) {
     const [selectedRoom, setSelectedRoom] = useState([]);
     const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+    const { dates } = useContext(SearchContext);
+
+    const getDatesInRange = (startDate, endDate) => {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const date = new Date(start.getTime());
+
+        let list = [];
+
+        while (date <= end) {
+            list.push(new Date(date).getTime());
+            date.setDate(date.getDate() + 1);
+        }
+
+        return list;
+    };
+
+    console.log(getDatesInRange(dates[0].startDate, dates[0].endDate));
 
     const handleSelect = (e) => {
         const checked = e.target.checked;
@@ -17,6 +36,7 @@ export default function Reserve({ setOpenReserve, hotelId }) {
                 : selectedRoom.filter((item) => item !== value) // when unchecked filter any value === item
         );
     };
+    const handleReserve = () => {};
     return (
         <div className="reserve">
             <div className="reserve-container">
@@ -27,14 +47,18 @@ export default function Reserve({ setOpenReserve, hotelId }) {
                 />
                 <span>Select you rooms: </span>
                 {data.map((item) => (
-                    <div className="room-item">
-                        <div className="room-item-info">
-                            <div className="room-item-title">{item.title}</div>
-                            <div className="room-desc">Room's description</div>
-                            <div className="room-max">
+                    <div className="reserve-item">
+                        <div className="reserve-item-info">
+                            <div className="reserve-item-title">
+                                {item.title}
+                            </div>
+                            <div className="reserve-desc">
+                                Room's description
+                            </div>
+                            <div className="reserve-max">
                                 Max people: <b>{item.maxPeople}</b>
                             </div>
-                            <div className="room-price">{item.price}</div>
+                            <div className="reserve-price">{item.price}</div>
                         </div>
                         {item.roomNumbers.map((roomNumber) => (
                             <div className="room">
@@ -48,6 +72,9 @@ export default function Reserve({ setOpenReserve, hotelId }) {
                         ))}
                     </div>
                 ))}
+                <button className="reserve-button" onClick={handleReserve}>
+                    Reserve Now!
+                </button>
             </div>
         </div>
     );
